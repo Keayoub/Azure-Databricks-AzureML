7ed652e1d74d870d0fbee87f850399d8dbff7aa1
Ayoub KEBAILI <keayoub@msn.com>
2026-02-05 19:59:21 -0500

    feat: Update outputs to reference data sources for storage account and access connector in UC metastore module

--- terraform/modules/adb-uc-metastore/outputs.tf
+++ terraform/modules/adb-uc-metastore/outputs.tf
@@ -10,20 +10,20 @@ output "metastore_name" {
 
 output "storage_account_id" {
   description = "ID of the storage account used by UC metastore"
-  value       = azurerm_storage_account.uc_metastore.id
+  value       = data.azurerm_storage_account.uc_metastore.id
 }
 
 output "storage_account_name" {
   description = "Name of the storage account used by UC metastore"
-  value       = azurerm_storage_account.uc_metastore.name
+  value       = data.azurerm_storage_account.uc_metastore.name
 }
 
 output "access_connector_id" {
   description = "ID of the Databricks access connector"
-  value       = azurerm_databricks_access_connector.uc_connector.id
+  value       = data.azurerm_databricks_access_connector.uc_connector.id
 }
 
 output "access_connector_principal_id" {
   description = "Principal ID of the managed identity for access connector"
-  value       = azurerm_databricks_access_connector.uc_connector.identity[0].principal_id
+  value       = data.azurerm_databricks_access_connector.uc_connector.identity[0].principal_id
 }
